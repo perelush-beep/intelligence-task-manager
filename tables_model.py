6796dd578e76f50740b682bc092592db9b1d0b99
Aharon Perel <perelush@gmail.com>
2026-06-17 17:20:29 +0300

    tables models placed in a seperate file for comfort and clean code

---
+++ tables_model.py
@@ -0,0 +1,23 @@
+agents = """
+CREATE TABLE agents(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50),
+speciality VARCHAR(50),
+is_active BOOLEAN DEFAULT TRUE,
+completed_missions INT DEFAILT 0,
+failed_missions INT DEFAULT 0,
+agent_rank ENUM("Junior", "Senior", "Commander")
+);"""
+
+mission = """
+CREATE TABLE mission(
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(100),
+descriptoin TEXT,
+location VARCHAR(50),
+difficulty INT,
+importance INT,
+status VARCHAR(15) DEFAULT NEW,
+risk_level VARCHAR(8),
+assigned_agent_id INT DEFAULT NULL)
+"""
